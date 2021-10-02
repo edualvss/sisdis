b20501e479d9e2d7d05ac73c9f615f562a006312
Eduardo Alves da Silva <eas@univali.br>
2021-10-02 17:26:33 -0300

    :sparkles: Adição da fila de mensagens

---
+++ MessageQueue/rabbitmq_consumer.py
@@ -0,0 +1,31 @@
+import pika, os, time
+
+def pdf_process_function(msg):
+    print(" Processamento do PDF ")
+    print(" [x] Mensagem recebida: "+ str(msg))
+
+    time.sleep(5) # Atraso de 5 segundos
+    print(" Processamento do PDF finalizado");
+    return;
+
+# Access the CLOUDAMQP_URL environment variable and parse it (fallback to RabbitMQ broker / localhost / cloudamqp
+url = os.environ.get('CLOUDAMQP_URL','amqps://tqzvyxix:oGh4wcsnvBrG2U5zzQBDgkcFH_ff8B8C@jackal.rmq.cloudamqp.com/tqzvyxix')
+params = pika.URLParameters(url)
+
+connection = pika.BlockingConnection(params)
+channel = connection.channel() # Inicia um canal
+channel.queue_declare(queue='pdfprocess')
+
+# Função a ser chamada quando uma mensagem chega
+def processa_msg(ch,method,properties,body):
+    pdf_process_function(body)
+
+
+# Configura a assinatura (subscribe) na fila
+channel.basic_consume('pdfprocess', processa_msg, auto_ack=True)
+
+
+# Inicia o consumo (blocos)
+channel.start_consuming()
+
+connection.close()
---
+++ MessageQueue/rabbitmq_publisher.py
@@ -0,0 +1,19 @@
+import pika, os, logging, sys
+
+logging.basicConfig()
+
+# Parse CLOUDAMQP_URL (fallback to RabbitMQ broker / localhost / cloudamqp)
+url = os.environ.get('CLOUDAMQP_URL','amqps://tqzvyxix:oGh4wcsnvBrG2U5zzQBDgkcFH_ff8B8C@jackal.rmq.cloudamqp.com/tqzvyxix')
+params = pika.URLParameters(url)
+params.socket_timeout = 5 # Altera o timeout de 0.25s para 5s evitar problemas de conexoes lentas
+
+# Estabelecendo a conexao, o canal e a fila
+connection = pika.BlockingConnection(params)
+channel = connection.channel()
+channel.queue_declare(queue='pdfprocess')
+
+# Enviando uma mensagem
+channel.basic_publish(exchange='',routing_key='pdfprocess',body=sys.argv[1])
+print("[x] Mensagem enviada para o consumidor")
+
+connection.close()
