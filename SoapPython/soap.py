6843a6b27917d30033acc21ccaf506a3fdb088d3
Eduardo Alves da Silva <eas@univali.br>
2021-10-19 01:37:10 -0300

    :sparkles: Add Python Soap Client

---
+++ SoapPython/soap.py
@@ -0,0 +1,8 @@
+from zeep import Client
+
+client = Client("http://www.soapclient.com/xml/soapresponder.wsdl")
+result = client.service.Method1(bstrParam1='Oi', bstrParam2='SisDis')
+#result = client.service.Method1('Oi', 'SisDis')
+
+print(result)
+
